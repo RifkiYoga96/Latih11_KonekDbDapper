7e93b81e4e682acce12c3224494b62b87c10c935
Rifki Yoga <rys9612345@gmail.com>
2024-07-12 07:54:15 +0700

    kirim

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,21 +30,23 @@
         {
             dataGridView1 = new DataGridView();
             button1 = new Button();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Location = new Point(14, 63);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(781, 412);
+            dataGridView1.Size = new Size(781, 376);
             dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(675, 442);
+            button1.Location = new Point(675, 450);
             button1.Name = "button1";
             button1.Size = new Size(118, 38);
             button1.TabIndex = 1;
@@ -52,22 +54,45 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(79, 24);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(177, 33);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Nilai";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(846, 500);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
         private Button button1;
+        private ComboBox comboBox1;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,16 +3,45 @@ namespace Latih11_KonekDbDapper
     public partial class Form1 : Form
     {
         private readonly NilaiDal nilaiDal;
+        public List<string> list;
         public Form1()
         {
             InitializeComponent();
             nilaiDal = new NilaiDal();
+            list = new List<string>()
+            {
+                "All","Diatas KKM","Dibawah KKM"
+            };
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             dataGridView1.DataSource = nilaiDal.ListData();
-            
+
+            comboBox1.DataSource = list;
         }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Ngelist();
+        }
+
+        public void Ngelist()
+        {
+            var a = nilaiDal.ListData();
+            int nilai = 0;
+            if (comboBox1.SelectedIndex == 1)
+            {
+                nilai = 75;
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                var fil = a.Where(x => x.Nilai < 75).ToList();
+                dataGridView1.DataSource = fil;
+                return;
+            }
+            var filter = a.Where(x => x.Nilai >= nilai).ToList();
+            dataGridView1.DataSource = filter;
+        }
+
     }
 }
